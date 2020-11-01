38dc52627ad6c43d1f30d4cafd3c681f63f0551c
jupiterbjy <jupiterbjy@gmail.com>
2020-11-01 22:53:22 +0900

    update

--- 64622547/64622547.py
+++ 64622547/64622547.py
@@ -8,6 +8,7 @@ def parse_rgb_gen(filename="source.txt"):
         for line in fp:
             yield "\t".join(line.split())
 
+
 def wrapped():
     for item in parse_rgb_gen():
         repr(item)
@@ -23,6 +24,7 @@ def parse_re_gen(filename):
             except AttributeError:  # first line falls here. " ! $Xorg:~~~ "
                 pass
 
+
 def wrapped_re():
     gen_instance = parse_re_gen('source.txt')
 
@@ -32,5 +34,6 @@ def wrapped_re():
 
 # res1 = f"without re: {timeit.timeit(parse_rgb_gen, number=100):.4}"
 # res2 = f"without re: {timeit.timeit(red_green_blue, number=100):.4}"
+
 
 wrapped_re()
