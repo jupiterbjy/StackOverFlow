3a7c5945af838e0c4efba81997edc20da16d7a13
jupiterbjy <jupiterbjy@gmail.com>
2020-12-18 20:04:55 +0900

    update

--- 65324776.py
+++ 65324776.py
@@ -1,37 +1,96 @@
-import typing as t
-
-T = t.TypeVar('T')
+from itertools import tee
 
 
-def split_on_condition_gen(seq, condition) -> t.Iterable[t.Iterable[T]]:
-    def inner_gen(range_: range):
-        for n in range_:
-            yield seq[n]
+def split_on_condition_gen(seq, condition):
+    seq_copy, seq_current = tee(seq)
+    counter = 0
+
+    def inner_gen(seq_, start, end):
+        nonlocal counter
+
+        for _ in range(counter, start):  # Skip from counter to start
+            next(seq_)
+
+        for _ in range(start, end):  # yield from start to end
+            yield next(seq_)
+
+            counter = end
 
     last = 0
-    current = 0
 
-    for item in seq:
+    for idx, item in enumerate(seq_current):
         if condition(item):
-            current += 1
-        else:
-            yield inner_gen(range(last, current))
-            last = current
+            continue
+
+        yield inner_gen(seq_copy, last, idx)
+        last = idx + 1
+
+    yield inner_gen(seq_copy, last, idx + 1)
 
 
-"""
->>> from this import s as seq_
-~~ ZEN OF PYTHON GOES HERE ~~
+class NonIndexAble:
+    def __init__(self, initial: str):
+        self.source = initial
+        self.list_ = list(initial)
 
->>> condition = lambda x: x != ','
->>> output = split_on_condition_gen(seq_, condition)
->>> output
-<generator object split_on_condition_gen at 0x000002602C1ECEB0>
+    def __iter__(self):
+        return self
 
->>> list_output = list(output)
->>> list_output[2]
-<generator object split_on_condition_gen.<locals>.inner_gen at 0x000002602C1ECEB0>
+    def __next__(self):
+        try:
+            return self.list_.pop(0)
+        except IndexError:
+            raise StopIteration()
 
->>> list(list_output[2])
-['l', ',', ' ', 'e', 'r', 's', 'h', 'f', 'r', ' ', ...]
-"""
+
+def test_non_index_able():
+    """
+    >>> non_index_able = NonIndexAble('aabaaaba')
+    >>> condition = lambda x: x == "a"
+
+    >>> for part in split_on_condition_gen(non_index_able, condition):
+    ...     print("Part start")
+    ...     for item in part:
+    ...         print(item, end=' ')
+    ...     print("\\nEnd")
+    Part start
+    a a
+    End
+    Part start
+    a a a
+    End
+    Part start
+    a
+    End
+
+    >>> non_index_able = NonIndexAble('asdf adsfa ds sdfdf adf')
+    >>> condition = lambda x: x != ' '
+
+    >>> for part in split_on_condition_gen(non_index_able, condition):
+    ...     print("Part start")
+    ...     for item in part:
+    ...         print(item, end=' ')
+    ...     print("\\nEnd")
+    Part start
+    a s d f
+    End
+    Part start
+    a d s f a
+    End
+    Part start
+    d s
+    End
+    Part start
+    s d f d f
+    End
+    Part start
+    a d f
+    End
+
+    """
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    # Why this fails? how?
